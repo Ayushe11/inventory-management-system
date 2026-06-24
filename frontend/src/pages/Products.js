330b18c42f7648ad5e43d6207ec2df09d93ea9af
Dev <dev@inventory.local>
2026-06-25 02:41:23 +0530

    Remove unused Alert import

--- frontend/src/pages/Products.js
+++ frontend/src/pages/Products.js
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
-import { Table, Alert } from 'react-bootstrap';
+import { Container, Row, Col, Card, Button, Table, Badge, Form } from 'react-bootstrap';
 import { fetchProducts, deleteProduct, resetSuccess } from '../redux/slices/productSlice';
 import ProductForm from './ProductForm';
 
